79525530330a6948e15ccd117e655d1932349d8c
KJLinares <32548505+KJLinares@users.noreply.github.com>
2018-03-23 11:12:51 -0400

    classes creation

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # DanceSchool
+
+
+Provides a solution for dance studios that wish to manage the courses, schedules, teacchers and clients(students).
---
+++ classes/database.php
@@ -0,0 +1,38 @@
+<?php
+
+
+
+include "dbConfig.php";
+class Database{
+	
+	public $Connection;
+	
+	function __construct(){
+		
+		$this->OpenConnection();
+	}
+	function __destruct(){
+		
+		$this->CloseConnection();
+	}
+	function OpenConnection(){
+		global $HOST , $USER , $PASSWORD , $DB_NAME;
+		
+		try{
+			$this->Connection =  new PDO("mysql:host=$HOST; dbname=$DB_NAME" ,
+			                             $USER , $PASSWORD);
+			$this->Connection->setAttribute(PDO::ATTR_ERRMODE , 
+			                                PDO::ERRMODE_EXCEPTION);
+			
+			
+		}catch(PDOException $e){
+			echo "Connection Failed ".$e->getMessage();
+		}
+		
+	}
+	function CloseConnection(){
+		$this->Connection = null;
+	}
+}
+
+?>
