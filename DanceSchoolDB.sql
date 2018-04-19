4d1b101fc0572ca5886f173bfa772770a7febcae
KJLinares <32548505+KJLinares@users.noreply.github.com>
2018-04-19 16:44:08 -0400

    Database update

--- DanceSchoolDB.sql
+++ DanceSchoolDB.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Apr 15, 2018 at 12:52 AM
+-- Generation Time: Apr 19, 2018 at 10:39 PM
 -- Server version: 10.1.28-MariaDB
 -- PHP Version: 5.6.32
 
@@ -49,6 +49,13 @@ CREATE TABLE `course` (
   `employee_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `course`
+--
+
+INSERT INTO `course` (`course_id`, `name`, `start_date`, `end_date`, `schedule`, `price`, `employee_id`) VALUES
+(1, 'Salsa', '2018-04-13 00:00:00', '2018-06-13 00:00:00', 'Monday 10:00 Am', 24, 1);
+
 -- --------------------------------------------------------
 
 --
@@ -64,6 +71,14 @@ CREATE TABLE `employee` (
   `username` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `employee`
+--
+
+INSERT INTO `employee` (`employee_id`, `name`, `email`, `phone`, `preferred_schedule`, `username`) VALUES
+(1, 'juli', 'juli@juli', 'juli', 'juli', 'juli'),
+(2, 'Admin', 'Admin', 'Admin', 'Admin', 'Admin');
+
 -- --------------------------------------------------------
 
 --
@@ -77,6 +92,13 @@ CREATE TABLE `student` (
   `phone` varchar(45) NOT NULL,
   `username` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `student`
+--
+
+INSERT INTO `student` (`student_id`, `name`, `email`, `phone`, `username`) VALUES
+(5, 'Elena Encheva', 'eli@yahoo.com', '76909767', 'eli');
 
 -- --------------------------------------------------------
 
@@ -102,6 +124,15 @@ CREATE TABLE `user` (
   `user_type` varchar(45) NOT NULL,
   `salt` varchar(45) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`username`, `password`, `user_type`, `salt`) VALUES
+('Admin', '$2y$10$e3afed0047b08059d0fadOnqpRoeSZR9er7ayX0p98dRv5vCXHITm', 'Manager', '$2y$10$e3afed0047b08059d0fada$'),
+('eli', '$2y$10$0a57258559de00695ffb0ePcz/mp3CW5CL0uRLik6DwceSojNVH3.', 'Student', '$2y$10$0a57258559de00695ffb0f$'),
+('juli', '$2y$10$4c37dbfae76a9a48544d7uKJRA0tT34Ekhacym6eiUvGucJzvFBWC', 'Teacher', '$2y$10$4c37dbfae76a9a48544d72$');
 
 --
 -- Indexes for dumped tables
@@ -157,19 +188,19 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `course`
 --
 ALTER TABLE `course`
-  MODIFY `course_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `course_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `employee`
 --
 ALTER TABLE `employee`
-  MODIFY `employee_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `employee_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `student`
 --
 ALTER TABLE `student`
-  MODIFY `student_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `student_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- Constraints for dumped tables
